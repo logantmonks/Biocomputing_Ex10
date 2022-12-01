fba1025b0bcf5974504d41a8e4728afdc7e183bb
Sarah Chong <schong3@nd.edu>
2022-12-01 14:26:16 -0500

    Almost done

--- Ex10.R
+++ Ex10.R
@@ -6,35 +6,63 @@
 GameData<-read.table("UWvMSU_1-22-13.txt", header=TRUE, sep="",stringsAsFactors=FALSE)
 
 # create variables to hold the team scores
-sumUW<-0
-sumMSU<-0
+sumUW[1]<-0
+sumMSU[1]<-0
+
 
 for(i in 1:nrow(GameData)){
   if(GameData$team[i]=="UW"){
-    sumUW<-sumUW+GameData$score[i]
+   sumUW[i+1]<-sumUW[i]+GameData$score[i]
+   sumMSU[i+1]<-sumMSU[i]
+    
+    
   
   }else{
-    sumMSU<-sumMSU+GameData$score[i]
+    sumMSU[i+1]<-sumMSU[i]+GameData$score[i]
+    sumUW[i+1]<-sumUW[i]
   }
 }
+
+FinalData<-data.frame(time=c(0, GameData$time),MSUscore=sumMSU, UWscore=sumUW)
 #how do I get the score for every time increment to show up???!!!ask
 
-ggplot(data=GameData, x="time", y="score")
+ggplot(data=FinalData, aes(x=time, y=UWscore))+
 #type of graph that we need
-geom_smooth()
-
+geom_line(aes(x=time, y=UWscore))+
+geom_line(aes(x=time, y=MSUscore))
 #Question2
 
-ComputerNumber<-rnorm(n=1, mean=50, sd=3)
-For(i in Guess.csv){
-  if(i>ComputerNumber){
-    print("lower")
-  }else if(i<ComputerNumber){
-    print("higher")
-  }else if(i==ComputerNumber){
+ComputerNumber<-sample(x=1:100,size=1)
+
+word = readline(prompt="Guess")
+
+
+Correct<-FALSE
+
+  
+for(i in 1:10){
+  if(Correct==FALSE){
+    #store guesses
+    Answers<-as.numeric(readline(prompt="Guess"))
+    #incorrect baseline
+    #guess to begin with
+    #keep guessing if wrong/until right
+    
+    
+    if(Answers>ComputerNumber){
+      print("lower")
+    }else if(Answers<ComputerNumber){
+      print("higher")
+    }else if(Answers==ComputerNumber){
       print("Correct!")
+      Correct==TRUE
     }
+  }
+  
+  
+
 }
+
 
 #How do I only pick integers??
 #How do I input guesses?
