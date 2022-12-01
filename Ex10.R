414626dc3bfafafa09594a1f1c9dc0387da9f40a
Sarah Chong <schong3@nd.edu>
2022-12-01 12:55:22 -0500

    Save progress on Q1

--- Ex10.R
+++ Ex10.R
@@ -2,10 +2,25 @@
 #Goal of project: Plot scores of each team over the course of the game
 
 #basic plot requires data, aesthetic, and geom structure
-#create dataframe
-GameData<-read.csv("UWvMSU_1-22-13.txt", header=TRUE, sep="")
+#load data 
+GameData<-read.table("UWvMSU_1-22-13.txt", header=TRUE, sep="",stringsAsFactors=FALSE)
 
-#load data
-ggplot(data="UWvMSU_1-22-13.txt", x="time", y="score")
+# create variables to hold the team scores
+sumUW<-0
+sumMSU<-0
+
+for(i in 1:nrow(GameData)){
+  if(GameData$team[i]=="UW"){
+    sumUW<-sumUW+GameData$score[i]
+  
+  }else{
+    sumMSU<-sumMSU+GameData$score[i]
+  }
+}
+#how do I get the score for every time increment to show up???!!!ask
+
+ggplot(data=GameData, x="time", y="score")
 #type of graph that we need
 geom_smooth()
+
+#Question2
